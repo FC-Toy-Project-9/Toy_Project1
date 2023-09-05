9706d8fa13f06e00cccf043d39c305e7bde6b63c
JeongUijeong <jeong275117@gmail.com>
2023-09-05 15:21:37 +0900

    feat: TripDTO 생성

--- src/main/java/domain/trip/dto/TripDTO.java
+++ src/main/java/domain/trip/dto/TripDTO.java
@@ -1,5 +1,36 @@
 package domain.trip.dto;
 
+import com.google.gson.annotations.SerializedName;
+import domain.itinerary.dto.ItineraryDTO;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TripDTO {
 
+    @SerializedName("trip_id")
+    private int id;
+    @SerializedName("trip_name")
+    private String name;
+    @SerializedName("start_date")
+    private LocalDate startDate;
+    @SerializedName("end_date")
+    private LocalDate endDate;
+    @SerializedName("itineraries")
+    private List<ItineraryDTO> itineraries;
+
+    @Builder
+    public TripDTO(int id, String name, LocalDate startDate, LocalDate endDate,
+        List<ItineraryDTO> itineraries) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.itineraries = itineraries;
+    }
 }
