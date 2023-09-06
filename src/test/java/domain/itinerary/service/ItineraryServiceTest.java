d8fb43fb2e4e9a9a7bf871b1ca99208ee4fbda5a
Yurim Yang <yang980329@likelion.org>
2023-09-07 00:58:25 +0900

    feat: 여정 기록 조회 기능 구현 - getItineraryListFromTrip() : 특정 여행의 여정을 조회

---
+++ src/test/java/domain/itinerary/service/ItineraryServiceTest.java
@@ -0,0 +1,65 @@
+package domain.itinerary.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import domain.itinerary.dto.ItineraryDTO;
+import domain.trip.dto.TripDTO;
+import domain.trip.exception.TripFileNotFoundException;
+import domain.trip.service.TripService;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ItineraryServiceTest {
+
+    TripService tripService = new TripService();
+    ItineraryService itineraryService = new ItineraryService();
+
+    @Mock
+    ItineraryService mockItineraryService = new ItineraryService();
+
+    @Mock
+    TripService mockTripService = new TripService();
+
+    @Nested
+    @DisplayName("getItineraryListFromTrip()은")
+    class Context_getItineraryListFromTrip {
+
+        @Test
+        @DisplayName("특정 여행의 여정 기록을 조회할 수 있다.")
+        void _willSuccess() throws FileNotFoundException {
+            //given
+            List<ItineraryDTO> itineraryList = new ArrayList<>();
+            itineraryList.add(ItineraryDTO.builder().id(1).build());
+            //when(mockItineraryService.getItineraryListFromTrip(1)).thenReturn(itineraryList);
+
+            //when
+            List<ItineraryDTO> result = itineraryService.getItineraryListFromTrip(1);
+
+
+            //then
+            Assertions.assertEquals(result.get(0).getId(), itineraryList.get(0).getId());
+        }
+
+        @Test
+        @DisplayName("특정 여행 기록이 없으면 조회할 수 없다.")
+        void tripFileNotFound_willFail() {
+            Throwable exception = assertThrows(TripFileNotFoundException.class, () -> {
+                tripService.getTripFromJson(3);
+            });
+            assertEquals("여행 파일을 찾을 수 없습니다.", exception.getMessage());
+        }
+
+    }
+
+}
