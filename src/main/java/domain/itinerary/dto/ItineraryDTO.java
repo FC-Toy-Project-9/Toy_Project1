1cab1d6907106efd923e9ca186893574981e02c4
minjung <ypd06021@naver.com>
2023-09-07 02:03:38 +0900

    feat: 여정 기록을 위한 DTO 구현

--- src/main/java/domain/itinerary/dto/ItineraryDTO.java
+++ src/main/java/domain/itinerary/dto/ItineraryDTO.java
@@ -1,5 +1,18 @@
 package domain.itinerary.dto;
 
-public class ItineraryDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+import java.time.LocalDateTime; // 날짜와 시간을 다루기 위한 패키지 추가
+
+@Getter
+@AllArgsConstructor
+public class ItineraryDTO {
+    private int itineraryId;             // 여정 ID
+    private String departurePlace;       // 출발지
+    private String destination;          // 도착지
+    private LocalDateTime departureTime; // 출발 시간
+    private LocalDateTime arrivalTime;   // 도착 시간
+    private LocalDateTime checkIn;       // 체크인 시간
+    private LocalDateTime checkOut;      // 체크아웃 시간
 }
