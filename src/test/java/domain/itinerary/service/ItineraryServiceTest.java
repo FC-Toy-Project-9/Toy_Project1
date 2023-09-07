8239d690bf762343319bf8685880fa895fc18d66
Yurim Yang <yang980329@likelion.org>
2023-09-08 03:10:53 +0900

    feat: 여정 조회 및 삭제 api test완료 - Context_getItineraryListFromCSV - Context_deleteItineraryFromCSV

--- src/test/java/domain/itinerary/service/ItineraryServiceTest.java
+++ src/test/java/domain/itinerary/service/ItineraryServiceTest.java
@@ -2,6 +2,7 @@ package domain.itinerary.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import domain.itinerary.dto.ItineraryDTO;
@@ -9,7 +10,10 @@ import domain.itinerary.exception.ItineraryNotFoundException;
 import domain.trip.dto.TripDTO;
 import domain.trip.exception.TripFileNotFoundException;
 import domain.trip.service.TripService;
+import global.dto.TripCsvDTO;
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
@@ -25,12 +29,9 @@ public class ItineraryServiceTest {
 
     TripService tripService = new TripService();
     ItineraryService itineraryService = new ItineraryService();
-
     @Mock
     ItineraryService mockItineraryService = new ItineraryService();
 
-    @Mock
-    TripService mockTripService = new TripService();
 
     @Nested
     @DisplayName("getItineraryListFromJson()은")
@@ -84,5 +85,45 @@ public class ItineraryServiceTest {
         }
     }
 
+    @Nested
+    @DisplayName("getItineraryFromCsv()는 ")
+    class Context_getItineraryListFromCSV {
+
+        @Test
+        @DisplayName("특정 여행의 여정 기록을 조회할 수 있다.")
+        void _willSuccess() throws FileNotFoundException {
+            //given
+            ItineraryDTO itinerary = TripCsvDTO.builder()
+                .tripId(1).tripName("Family Vacation")
+                .startDate(LocalDate.parse("2023-08-15", DateTimeFormatter.ISO_DATE))
+                .endDate(LocalDate.parse("2023-08-25", DateTimeFormatter.ISO_DATE))
+                .itineraryId(5).build().toItineraryDTO();
+            List<ItineraryDTO> itineraryList = new ArrayList<>();
+            itineraryList.add(itinerary);
+            lenient().when(mockItineraryService.getItineraryListFromCSV(1))
+                .thenReturn(itineraryList);
+
+            //when
+            List<ItineraryDTO> result = itineraryService.getItineraryListFromCSV(1);
+
+            //then
+            Assertions.assertEquals(result.get(0).getId(), itineraryList.get(0).getId());
+        }
+    }
+
+    @Nested
+    @DisplayName("deleteItineraryFromCSV()는 ")
+    class Context_deleteItineraryFromCSV {
+
+        @Test
+        @DisplayName("특정 여정을 삭제할 수 있다.")
+        void _willSuccess() throws ItineraryNotFoundException {
+            //given, when
+            boolean result = itineraryService.deleteItineraryFromCSV(1, 5);
+
+            //then
+            Assertions.assertTrue(result);
+        }
+    }
 
 }
