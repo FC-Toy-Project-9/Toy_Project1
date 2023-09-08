906035db6ff96211746e251eb2dd8c22848be071
JeongUijeong <jeong275117@gmail.com>
2023-09-08 17:56:09 +0900

    refactor: 리팩토링 및 기능 수정

--- src/main/java/domain/itinerary/exception/ItineraryNotFoundException.java
+++ src/main/java/domain/itinerary/exception/ItineraryNotFoundException.java
@@ -1,9 +1,9 @@
 package domain.itinerary.exception;
 
-public class ItineraryNotFoundException extends Throwable {
+public class ItineraryNotFoundException extends Exception {
 
     public ItineraryNotFoundException() {
-        System.out.println("해당 여정을 찾을 수 없습니다.");
+        super("해당 여정을 찾을 수 없습니다.");
     }
 
 }
